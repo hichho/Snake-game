a9ca645be100dadb120f137c5f7bbf7a08fb02ef
hichho <52339006+hichho@users.noreply.github.com>
2021-11-23 18:00:32 +0800

    complete food class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-console.log('11');
 import './style/index.less';
+
+//定义类
+
+class Food {
+    element: HTMLElement;
+
+    constructor() {
+        this.element = document.getElementById('food')!;
+    }
+
+    get X() {
+        return this.element.offsetLeft;
+    }
+
+    get Y() {
+        return this.element.offsetTop;
+    }
+
+    change() {
+        let top = Math.round(Math.random() * 29) * 10;
+        let left = Math.round(Math.random() * 29) * 10;
+        this.element.style.left = left + 'px';
+        this.element.style.top = top + 'px';
+    }
+}
+
+const food = new Food();
+console.log(food.X, food.Y);
+food.change();
+console.log(food.X, food.Y);
